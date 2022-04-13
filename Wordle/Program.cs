c3e1cccda2c360fe08fa82280fdaee26deaffd74
Sean Cullen <eksean@hotmail.com>
2022-04-13 21:54:44 +0100

    Add project files.

---
+++ Wordle/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Wordle;
+
+var wordProvider = new WordProvider();
+var allWords = wordProvider.GetWords();
+
+
+
+Console.Write("Enter a word: ");
+var GuessWord = Console.ReadLine();
+Console.WriteLine("You have guessed " + GuessWord);
---
+++ Wordle/WordProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Wordle
+{
+    internal class WordProvider
+    {
+        public List<string> GetWords()
+        {
+
+            List<string> allLinesText = File.ReadAllLines(@"C:\Users\eksea\source\repos\Wordle\Wordle\WordList.txt").ToList();
+
+
+            return allLinesText;
+        }
+    }
+}
